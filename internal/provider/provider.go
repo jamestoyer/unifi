8676c30ef8f335f88ba33a358ad265460400283b
James Toyer <jamestoyer@gmail.com>
2024-07-07 21:35:46 +0100

    Rename the provider

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -33,7 +33,7 @@ type ScaffoldingProviderModel struct {
 }
 
 func (p *ScaffoldingProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
-	resp.TypeName = "scaffolding"
+	resp.TypeName = "unifi"
 	resp.Version = p.version
 }
 
