f544e6f7d4c5489179d3dc74cf214d9c0057dbab
James Toyer <jamestoyer@gmail.com>
2024-07-13 10:49:41 +0100

    Add all fields

--- internal/provider/user_resource.go
+++ internal/provider/user_resource.go
@@ -12,10 +12,9 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/paultyng/go-unifi/unifi"
 )
 
@@ -34,7 +33,6 @@ type UserResource struct {
 
 // UserResourceModel describes the resource data model.
 type UserResourceModel struct {
-	Id           types.String `tfsdk:"id"`
 	MAC          types.String `tfsdk:"mac"`
 	Name         types.String `tfsdk:"name"`
 	UserGroupID  types.String `tfsdk:"user_group_id"`
@@ -43,7 +41,22 @@ type UserResourceModel struct {
 	NetworkID    types.String `tfsdk:"network_id"`
 	Blocked      types.Bool   `tfsdk:"blocked"`
 	DeviceIconID types.Int64  `tfsdk:"device_icon_id"`
-	SiteID       types.String `tfsdk:"site_id"`
+
+	// Computed
+	ID                            types.String `tfsdk:"id"`
+	Hidden                        types.Bool   `tfsdk:"attr_hidden"`
+	HiddenID                      types.String `tfsdk:"attr_hidden_id"`
+	NoDelete                      types.Bool   `tfsdk:"attr_no_delete"`
+	NoEdit                        types.Bool   `tfsdk:"attr_no_edit"`
+	IP                            types.String `tfsdk:"ip"` // non-generated field
+	FixedApEnabled                types.Bool   `tfsdk:"fixed_ap_enabled"`
+	FixedApMAC                    types.String `tfsdk:"fixed_ap_mac"` // ^([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})$
+	Hostname                      types.String `tfsdk:"hostname"`
+	LocalDNSRecord                types.String `tfsdk:"local_dns_record"`
+	LocalDNSRecordEnabled         types.Bool   `tfsdk:"local_dns_record_enabled"`
+	UseFixedIP                    types.Bool   `tfsdk:"use_fixedip"`
+	VirtualNetworkOverrideEnabled types.Bool   `tfsdk:"virtual_network_override_enabled"`
+	VirtualNetworkOverrideID      types.String `tfsdk:"virtual_network_override_id"`
 }
 
 func (r *UserResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
@@ -56,13 +69,6 @@ func (r *UserResource) Schema(ctx context.Context, req resource.SchemaRequest, r
 		MarkdownDescription: "Example resource",
 
 		Attributes: map[string]schema.Attribute{
-			"id": schema.StringAttribute{
-				Computed:            true,
-				MarkdownDescription: "Example identifier",
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
-			},
 			"mac": schema.StringAttribute{
 				Required:            true,
 				MarkdownDescription: "",
@@ -100,8 +106,65 @@ func (r *UserResource) Schema(ctx context.Context, req resource.SchemaRequest, r
 				Optional:            true,
 				MarkdownDescription: "ID of the the icon to assign to the device",
 			},
-			"site_id": schema.StringAttribute{
+
+			// Computed
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Example identifier",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"attr_hidden": schema.BoolAttribute{
 				Computed: true,
+				Default:  booldefault.StaticBool(false),
+			},
+			"attr_hidden_id": schema.StringAttribute{
+				Computed: true,
+				Default:  stringdefault.StaticString(""),
+			},
+			"attr_no_delete": schema.BoolAttribute{
+				Computed: true,
+				Default:  booldefault.StaticBool(false),
+			},
+			"attr_no_edit": schema.BoolAttribute{
+				Computed: true,
+				Default:  booldefault.StaticBool(false),
+			},
+			"ip": schema.StringAttribute{
+				Computed: true,
+				Default:  stringdefault.StaticString(""),
+			},
+			"fixed_ap_enabled": schema.BoolAttribute{
+				Computed: true,
+				Default:  booldefault.StaticBool(false),
+			},
+			"fixed_ap_mac": schema.StringAttribute{
+				Computed: true,
+				Default:  stringdefault.StaticString(""),
+			},
+			"hostname": schema.StringAttribute{
+				Computed: true,
+				Default:  stringdefault.StaticString(""),
+			},
+			"local_dns_record": schema.StringAttribute{
+				Computed: true,
+				Default:  stringdefault.StaticString(""),
+			},
+			"local_dns_record_enabled": schema.BoolAttribute{
+				Computed: true,
+				Default:  booldefault.StaticBool(false),
+			},
+			"use_fixedip": schema.BoolAttribute{
+				Computed: true,
+			},
+			"virtual_network_override_enabled": schema.BoolAttribute{
+				Computed: true,
+				Default:  booldefault.StaticBool(false),
+			},
+			"virtual_network_override_id": schema.StringAttribute{
+				Computed: true,
+				Default:  stringdefault.StaticString(""),
 			},
 		},
 	}
@@ -137,43 +200,15 @@ func (r *UserResource) Create(ctx context.Context, req resource.CreateRequest, r
 		return
 	}
 
-	user := &unifi.User{
-		Hidden:                        false,
-		HiddenID:                      "",
-		NoDelete:                      false,
-		NoEdit:                        false,
-		DevIdOverride:                 int(data.DeviceIconID.ValueInt64()),
-		IP:                            "",
-		Blocked:                       data.Blocked.ValueBool(),
-		FixedApEnabled:                false,
-		FixedApMAC:                    "",
-		FixedIP:                       data.FixedIP.ValueString(),
-		Hostname:                      "",
-		LastSeen:                      0,
-		LocalDNSRecord:                "",
-		LocalDNSRecordEnabled:         false,
-		MAC:                           data.MAC.ValueString(),
-		Name:                          data.Name.ValueString(),
-		NetworkID:                     data.NetworkID.ValueString(),
-		Note:                          data.Note.ValueString(),
-		UseFixedIP:                    data.FixedIP.ValueString() != "",
-		UserGroupID:                   data.UserGroupID.ValueString(),
-		VirtualNetworkOverrideEnabled: false,
-		VirtualNetworkOverrideID:      "",
-	}
-
+	user := convertUserResourceModel(data)
 	user, err := r.client.CreateUser(ctx, r.client.site, user)
 	if err != nil {
 		resp.Diagnostics.AddError("Create user error", fmt.Sprintf("Unable to create user, got error: %s", err))
 		return
 	}
 
-	data.Id = types.StringValue(user.ID)
-	data.SiteID = types.StringValue(user.SiteID)
-
-	// Write logs using the tflog package
-	// Documentation: https://terraform.io/plugin/log
-	tflog.Trace(ctx, "created a resource")
+	data.ID = types.StringValue(user.ID)
+	data = setComputedUserAttributes(user, data)
 
 	// Save data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
@@ -189,7 +224,7 @@ func (r *UserResource) Read(ctx context.Context, req resource.ReadRequest, resp 
 		return
 	}
 
-	user, err := r.client.GetUser(ctx, r.client.site, data.Id.ValueString())
+	user, err := r.client.GetUser(ctx, r.client.site, data.ID.ValueString())
 	if err != nil {
 		var notFoundError *unifi.NotFoundError
 		if errors.As(err, &notFoundError) {
@@ -201,16 +236,8 @@ func (r *UserResource) Read(ctx context.Context, req resource.ReadRequest, resp 
 		return
 	}
 
-	data.Blocked = basetypes.NewBoolValue(user.Blocked)
-	data.MAC = types.StringValue(user.MAC)
-
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-	// httpResp, err := r.client.Do(httpReq)
-	// if err != nil {
-	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read example, got error: %s", err))
-	//     return
-	// }
+	data = setComputedUserAttributes(user, data)
+	data = setUserDefinedUserAttributes(user, data)
 
 	// Save updated data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
@@ -226,13 +253,21 @@ func (r *UserResource) Update(ctx context.Context, req resource.UpdateRequest, r
 		return
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-	// httpResp, err := r.client.Do(httpReq)
-	// if err != nil {
-	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to update example, got error: %s", err))
-	//     return
-	// }
+	user := convertUserResourceModel(data)
+	user.ID = data.ID.ValueString()
+	user, err := r.client.UpdateUser(ctx, r.client.site, user)
+	if err != nil {
+		var notFoundError *unifi.NotFoundError
+		if errors.As(err, &notFoundError) {
+			resp.State.RemoveResource(ctx)
+			return
+		}
+
+		resp.Diagnostics.AddError("Read user error", fmt.Sprintf("Unable to read user, got error: %s", err))
+		return
+	}
+
+	data = setComputedUserAttributes(user, data)
 
 	// Save updated data into Terraform state
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
@@ -252,23 +287,66 @@ func (r *UserResource) Delete(ctx context.Context, req resource.DeleteRequest, r
 	if err != nil {
 		var notFoundError *unifi.NotFoundError
 		if errors.As(err, &notFoundError) {
-			resp.State.RemoveResource(ctx)
 			return
 		}
 
 		resp.Diagnostics.AddError("Delete user error", fmt.Sprintf("Unable to delete user, got error: %s", err))
 		return
 	}
-
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-	// httpResp, err := r.client.Do(httpReq)
-	// if err != nil {
-	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to delete example, got error: %s", err))
-	//     return
-	// }
 }
 
 func (r *UserResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
 	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
+
+func convertUserResourceModel(data UserResourceModel) *unifi.User {
+	user := &unifi.User{
+		DevIdOverride: int(data.DeviceIconID.ValueInt64()),
+		Blocked:       data.Blocked.ValueBool(),
+		FixedIP:       data.FixedIP.ValueString(),
+		MAC:           data.MAC.ValueString(),
+		Name:          data.Name.ValueString(),
+		NetworkID:     data.NetworkID.ValueString(),
+		Note:          data.Note.ValueString(),
+		UseFixedIP:    data.FixedIP.ValueString() != "",
+		UserGroupID:   data.UserGroupID.ValueString(),
+	}
+
+	return user
+}
+
+func setUserDefinedUserAttributes(user *unifi.User, data UserResourceModel) UserResourceModel {
+	data.MAC = types.StringValue(user.MAC)
+	data.Name = types.StringValue(user.Name)
+	data.UserGroupID = types.StringValue(user.UserGroupID)
+	data.Note = types.StringValue(user.Note)
+	data.FixedIP = types.StringValue(user.FixedIP)
+	data.NetworkID = types.StringValue(user.NetworkID)
+	data.Blocked = types.BoolValue(user.Blocked)
+
+	if user.DevIdOverride == 0 {
+		data.DeviceIconID = types.Int64Null()
+	} else {
+		data.DeviceIconID = types.Int64Value(int64(user.DevIdOverride))
+	}
+
+	return data
+}
+
+func setComputedUserAttributes(user *unifi.User, data UserResourceModel) UserResourceModel {
+	data.Hidden = types.BoolValue(user.Hidden)
+	data.HiddenID = types.StringValue(user.HiddenID)
+	data.NoDelete = types.BoolValue(user.NoDelete)
+	data.NoEdit = types.BoolValue(user.NoEdit)
+	data.IP = types.StringValue(user.IP)
+	data.FixedApEnabled = types.BoolValue(user.FixedApEnabled)
+	data.FixedApMAC = types.StringValue(user.FixedApMAC)
+	data.Hostname = types.StringValue(user.Hostname)
+	data.LocalDNSRecord = types.StringValue(user.LocalDNSRecord)
+	data.LocalDNSRecordEnabled = types.BoolValue(user.LocalDNSRecordEnabled)
+	data.UseFixedIP = types.BoolValue(user.UseFixedIP)
+	data.VirtualNetworkOverrideEnabled = types.BoolValue(user.VirtualNetworkOverrideEnabled)
+	data.VirtualNetworkOverrideID = types.StringValue(user.VirtualNetworkOverrideID)
+
+	return data
 }
